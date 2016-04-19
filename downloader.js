fd23ec64c694c093a4401f5f4a6e92f584d43e09
ProGM <progiemmeh@gmail.com>
2016-04-19 17:21:30 +0200

    Cleanup.

--- downloader.js
+++ downloader.js
@@ -28,22 +28,6 @@ module.exports = (function() {
         success(site_response);
       }
     });
-
-    // request.head(uri, function(err, res, body){
-    //   console.log('content-type:', res.headers['content-type']);
-    //   console.log('content-length:', res.headers['content-length']);
-
-    //   // Accept:text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*;q=0.8
-    //   // Accept-Encoding:gzip, deflate, sdch
-    //   // Accept-Language:it-IT,it;q=0.8,en-US;q=0.6,en;q=0.4
-    //   // Cache-Control:no-cache
-    //   // Connection:keep-alive
-    //   // Host:comefare.com
-    //   // Pragma:no-cache
-    //   // Referer:https://www.google.it/
-    //   // Upgrade-Insecure-Requests:1
-    //   // User-Agent:Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.112 Safari/537.36
-    // });
   }
   function download_temp(uri, success, failure) {
     var filename = "./tmp/" + uuid.v1() + ".tmp";
@@ -52,19 +36,6 @@ module.exports = (function() {
       function(res) {
         success(filename, res.headers['content-type']);
       }, failure);
-    // request.head(uri, function(err, res, body){
-    //   console.log('status code:', res.statusCode);
-    //   console.log('content-type:', res.headers['content-type']);
-    //   console.log('content-length:', res.headers['content-length']);
-    //   if (res.statusCode == 200) {
-    //     request(uri).pipe(fs.createWriteStream(filename)).on('close', function() {
-    //       console.log()
-    //       success(filename, res.headers['content-type'])
-    //     });
-    //   } else {
-    //     failure(res, body);
-    //   }
-    // });
   }
 
   return {
